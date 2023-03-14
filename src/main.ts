01c337793885e8b6992b9f3f37d011e615d02720
luismendes070 <machadoluismendes@gmail.com>
2023-03-14 12:36:00 -0300

    lubuntu

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,32 @@ import { bootstrapApplication } from '@angular/platform-browser';
 })
 export class App {
   name = 'Angular';
-}
+
+  constructor(){
+
+    try{
+      var parser, xmlDoc;
+      var text =
+        "<bookstore><book>" +
+        "<title>Everyday Italian</title>" +
+        "<author>Giada De Laurentiis</author>" +
+        "<year>2005</year>" +
+        "</book></bookstore>";
+
+      parser = new DOMParser();
+      xmlDoc = parser.parseFromString(text, "text/xml");
+
+      document.getElementById("demo").innerHTML =
+        xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    
+    }catch(error){
+      console.log('App class exception.');
+    }finally{
+      console.log('App class finally.');
+    }
+
+  } //end class
+
+} // end class App
 
 bootstrapApplication(App);
